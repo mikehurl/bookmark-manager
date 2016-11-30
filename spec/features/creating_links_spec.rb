6d622e6daf777c8df77c1523dcf51610d999cdaf
Dines Poulsen <dinespoulsen84@gmail.com>
2016-11-30 10:34:41 +0000

    Added new links page

---
+++ spec/features/creating_links_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+require './app/models/link.rb'
+
+feature 'Adding links', :type => :feature do
+
+  scenario 'I can add links to my list' do
+    visit '/links'
+    click_button('Add Link')
+    fill_in('title', :with => 'Dog gif page')
+    fill_in('url', :with => "http://www.doggifpage.com/")
+    click_button('Create Link')
+    expect(page).to have_content('Dog gif page')
+  end
+end
